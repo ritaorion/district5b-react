33f7fe0e1b9ca945fa2b20cf5103d80b9076120d
Andrew Godby <andrewmgodby@gmail.com>
2025-03-22 00:44:51 -0700

    UD Build Script

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd /home/master/applications/afpxvzvday/
+cd /home/master/applications/afpxvzvday/public_html/
 npm install
 npm run build
-mv public_html public_html.bak
-mkdir -p public_html
-cp -r dist/* public_html/
-echo "Build completed and deployed to public_html"
+cp -rf dist/* .
+rm -rf dist
+echo "Build completed and files deployed to public_html"
