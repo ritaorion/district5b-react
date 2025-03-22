d8a54bb481b8fb1b24b1c9da2d79eeb9a350a9de
Andrew Godby <andrewmgodby@gmail.com>
2025-03-22 00:54:03 -0700

    Downgrade to React 18.3.1

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/master/applications/afpxvzvday/public_html/
-npm install
-npm run build
-cp -rf dist/* .
-rm -rf dist
-echo "Build completed and files deployed to public_html"
